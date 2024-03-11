596b6646b4b885a3e5adebab7d3d18052b8b5b4f
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-03-11 12:20:59 -0100

    class work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NavbarpageComponent } from './navbarpage/navbarpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, LandingpageComponent,NavbarpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'First_Project';
+  // public,private,protected
+  // object: property and function
+  // typescript
 }
---
+++ src/app/navbarpage/navbarpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbarpage.component.html',
+  styleUrl: './navbarpage.component.css'
+})
+export class NavbarpageComponent {
+
+}
